2cc8ccf41c042832fe2502c2904e04cd3cbd0d46
meilingt99 <meilinglthompson@gmail.com>
2018-01-19 13:52:09 -0800

    made QuickUnion weighted

--- QuickUnion.java
+++ QuickUnion.java
@@ -1,9 +1,14 @@
 public class QuickUnion{
     private int[] id;
+    private int[] sizes;
     public QuickUnion( int n ){
         id = new int[n];
+        sizes = new int[n];
         for( int i = 0; i < id.length; i++ ){
             id[i] = i;
+        }
+        for( int i = 0; i < sizes.length; i++ ){
+            sizes[i] = 1;
         }
     }
     
@@ -19,6 +24,15 @@ public class QuickUnion{
     }
     
     public void union( int p, int q ){
-        id[root(p)] = root(q);
+        int root_p = root(p);
+        int root_q = root(q);
+        if( sizes[root_p] < sizes[root_q] ){
+            id[root_p] = root_q;
+            sizes[root_q] += sizes[root_p];
+        }
+        else{
+            id[root_q] = root_p;
+            sizes[root_p] += sizes[root_q];
+        }
     }
 }
